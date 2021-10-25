e20c656b06da07e9174756e6d4f5995a532a8a9e
Richard Hansen <rhansen@rhansen.org>
2021-10-25 05:17:18 -0400

    Changeset: Use `in` check to help TypeScript narrowing

--- src/static/js/Changeset.js
+++ src/static/js/Changeset.js
@@ -693,7 +693,7 @@ const textLinesMutator = (lines) => {
    * @returns {string}
    */
   const linesGet = (idx) => {
-    if (lines.get) {
+    if ('get' in lines) {
       return lines.get(idx);
     } else {
       return lines[idx];
