0b60631b347dcf10f8a15b49c8637bd986cf473d
Richard Hansen <rhansen@rhansen.org>
2021-10-25 01:21:19 -0400

    Changeset: Turn `newOp()` into a real class

--- src/node/utils/padDiff.js
+++ src/node/utils/padDiff.js
@@ -273,7 +273,7 @@ PadDiff.prototype._createDeletionChangeset = function (cs, startAText, apool) {
   let curChar = 0;
   let curLineOpIter = null;
   let curLineOpIterLine;
-  let curLineNextOp = Changeset.newOp('+');
+  let curLineNextOp = new Changeset.Op('+');
 
   const unpacked = Changeset.unpack(cs);
   const csIter = Changeset.opIterator(unpacked.ops);
